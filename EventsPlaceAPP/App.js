fa6d9a9e57eda6ba9bbc7ced61278c9e819e8d25
Jeison Visbal <visbalgomez@gmail.com>
2024-04-01 17:15:22 -0500

    Mejora items

--- EventsPlaceAPP/App.js
+++ EventsPlaceAPP/App.js
@@ -1,42 +1,60 @@
 import React from "react";
 import { StatusBar } from "expo-status-bar";
-import { Platform, StyleSheet, Text, View } from "react-native";
+import { Platform, StyleSheet, Text, View, Image } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 //import MenuWeb from "./src/menu/MenuWeb";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import {
+  createBottomTabNavigator,
+  focused,
+  color,
+} from "@react-navigation/bottom-tabs";
 import PerfilScreen from "./screens/Perfil";
 import NotificacionesScreen from "./screens/Notificaciones";
 import WallScreen from "./screens/Wall";
 import BoletasScreen from "./screens/Boletas";
 import ChatScreen from "./screens/Chat";
-
+import TabItems from "./constants/TabItems";
+import PropTypes from "prop-types";
 
 const Tab = createBottomTabNavigator();
 
-
-const MenuMovil = () =>{
+const MenuMovil = () => {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name="Notificaciones" component={NotificacionesScreen} />
-        <Tab.Screen name="Chat" component={ChatScreen} />
-        <Tab.Screen name="Wall" component={WallScreen} />
-        <Tab.Screen name="Mis Boletas" component={BoletasScreen} />
-        <Tab.Screen name="Mi Perfil" component={PerfilScreen} />
+      <Tab.Navigator initialRouteName="Wall">
+        {TabItems.map((Items) => (
+          <Tab.Screen
+            key={Items.name}
+            name={Items.name}
+            options={{
+              tabBarShowLabel: false,
+              tabBarActiveBackgroundColor: "#E4E5E5",
+              tabBarIcon: () => cargarIcono(Items.source),
+            }}
+            component={
+              Items.name === "Notificaciones"
+                ? NotificacionesScreen
+                : Items.name === "Chat"
+                ? ChatScreen
+                : Items.name === "Wall"
+                ? WallScreen
+                : Items.name === "Mis Boletas"
+                ? BoletasScreen
+                : PerfilScreen
+            }
+          />
+        ))}
       </Tab.Navigator>
     </NavigationContainer>
   );
-}
+};
 
-const Menu = () =>{
-  
-   if(Platform.OS === 'android') { 
-    return <MenuMovil />; 
-  } else{
-    
-    return (
-  <MenuMovil /> 
-  );}
+const Menu = () => {
+  if (Platform.OS === "android") {
+    return <MenuMovil />;
+  } else {
+    return <MenuMovil />;
+  }
 };
 
 export default function App() {
@@ -51,3 +69,15 @@ const styles = StyleSheet.create({
     justifyContent: "center",
   },
 });
+
+const cargarIcono = (imagen) => {
+  return (
+    <Image
+      source={imagen}
+      style={{
+        resizeMode: "contain",
+        tintColor: "#0F172A",
+      }}
+    />
+  );
+};
--- EventsPlaceAPP/constants/TabItems.js
+++ EventsPlaceAPP/constants/TabItems.js
@@ -1,27 +1,27 @@
 export default [
-    {
-        name: 'Mi Perfil',
-        source: '.assets/',
-        iconName: 'PerfilPic'
-    },
-    {
-        name: 'Mis Boletas',
-        source: '.assets/',
-        iconName: 'BoletasPic'
-    },
-    {
-        name: 'Notificaciones',
-        source: '.assets/',
-        iconName: 'NotificacionesPic'
-    },
-    {
-        name: 'Chat',
-        source: '.assets/',
-        iconName: 'ChatPic'
-    },
-    {
-        name: 'Wall',
-        source: '.assets/',
-        iconName: 'WallPic'
-    },
-]
+  {
+    name: "Notificaciones",
+    source: require("../assets/Icon_notifications.png"),
+    iconName: "NotificacionesPic",
+  },
+  {
+    name: "Chat",
+    source: require("../assets/Icon_chat.png"),
+    iconName: "ChatPic",
+  },
+  {
+    name: "Wall",
+    source: require("../assets/Icon_wall.png"),
+    iconName: "WallPic",
+  },
+  {
+    name: "Mis Boletas",
+    source: require("../assets/Icon_ticket.png"),
+    iconName: "BoletasPic",
+  },
+  {
+    name: "Mi Perfil",
+    source: require("../assets/Icon_profile.png"),
+    iconName: "PerfilPic",
+  },
+];
