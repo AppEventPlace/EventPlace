0e691e8ef284d022fb0b996f7185b88b9658887d
Jeison Visbal <visbalgomez@gmail.com>
2024-04-10 09:34:56 -0500

    Diseño pagina perfil

--- EventsPlaceAPP/screens/ScreenStackPerfil/PerfilPrincipal.js
+++ EventsPlaceAPP/screens/ScreenStackPerfil/PerfilPrincipal.js
@@ -1,17 +1,45 @@
 import React from "react";
-import { Text, View, StyleSheet } from "react-native";
+import { Text, View, StyleSheet, Image } from "react-native";
+import IconSVG from "../../assets/LogoSVG";
 
 const PerfilPrincipalScreen = () => {
   return (
     <View style={style.PerfilContainer}>
-      <Text> Mi perfil </Text>
+      <View style={style.Titulo}>
+        <Text style={style.Titulo}> Mi perfil </Text>
+      </View>
+      <View style={style.TarjetaPerfil}>
+        <View style={style.imagenPrincipal}>
+          <IconSVG theme={"FotoGenerica"} />
+        </View>
+      </View>
     </View>
   );
 };
+//const FotoPerfil = [require("../assets/Icon_profile.png")];
 
 const style = StyleSheet.create({
   PerfilContainer: {
-    backgroundColor: "#666666",
+    backgroundColor: "#F4F5FE",
+    width: "100%", // Ancho de a imagen
+    height: "100%", // Alto de la imagen
+    alignItems: "center",
+  },
+  TarjetaPerfil: {
+    backgroundColor: "white",
+    width: "95%",
+    marginTop: 50,
+    height: 200,
+  },
+  Titulo: {
+    alignContent: "center",
+
+    marginTop: 10,
+    fontWeight: "bold",
+  },
+  imagenPrincipal: {
+    marginLeft: 5,
+    marginTop: 15,
   },
 });
 
