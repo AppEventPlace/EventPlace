516a0fe48fdcdb0b5197be05e141e7a3bead02e8
Johann Casallas <casallasbecerrajohann@gmail.com>
2024-09-27 23:17:29 -0500

    se ajusta mapeo de datos en el frontend y filtro que descarta los gustos desabilitados

--- src/components/organisms/SeleccionTags.tsx
+++ src/components/organisms/SeleccionTags.tsx
@@ -1,47 +1,20 @@
 import React from "react";
-import { View, StyleProp, ViewStyle } from "react-native";
+import { View } from "react-native";
 import Seleccionable from "../Seleccionable";
 
 interface IPreference {
   preference_id: number;
-  preference_name: string;
+  preference_name_en: string;
+  preference_name_es: string;
   preference_status: boolean;
-  created_at: string;
-  updated_at: string;
+  preference_description: string;
 }
 
 interface SeleccionTagsProps {
   OpcSeleccion: IPreference[];
 }
 
-const preferenceLabels: Record<number, string> = {
-  1001: "Familia",
-  1002: "Comunidad y Cultura",
-  1003: "Iniciativa Social",
-  1004: "Autos, Barcos y Aeronáutica",
-  1005: "Cine, Medios y Entretenimiento",
-  1006: "Gobierno y Política",
-  1007: "Salud y Bienestar",
-  1008: "Pasatiempos e Intereses Especiales",
-  1009: "Música",
-  1010: "Artes Escénicas",
-  1011: "Moda y Belleza",
-  1012: "Comida y Bebidas",
-  1013: "Hogar y Estilo de Vida",
-  1014: "Actividades Escolares",
-  1015: "Educación",
-  1016: "Ciencia y Tecnología",
-  1017: "Deportes y Ejercicio",
-  1018: "Viajes y Actividades al Aire Libre",
-  1019: "Festivales y Actividades Estacionales",
-};
-
-const getLabelByPreferenceId = (preferenceId: number): string => {
-  return preferenceLabels[preferenceId] || "Unknown Preference";
-};
-
 const SeleccionTags: React.FC<SeleccionTagsProps> = ({ OpcSeleccion }) => {
-
   return (
     <View
       style={{
@@ -52,13 +25,15 @@ const SeleccionTags: React.FC<SeleccionTagsProps> = ({ OpcSeleccion }) => {
         width: "100%",
       }}
     >
-      {OpcSeleccion.map((Selec) => (
-        <Seleccionable
-          key={Selec.preference_id}
-          id={Selec.preference_id.toString()}
-          label={getLabelByPreferenceId(Selec.preference_id)}
-        />
-      ))}
+      {OpcSeleccion
+        .filter((Selec) => Selec.preference_status) 
+        .map((Selec) => (
+          <Seleccionable
+            key={Selec.preference_id}
+            id={Selec.preference_id.toString()}
+            label={Selec.preference_name_es} 
+          />
+        ))}
     </View>
   );
 };
